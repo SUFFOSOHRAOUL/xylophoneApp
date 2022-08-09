4415c7070235ebc71e7de038fc41ae82de850ea2
unknown <sohraoulsuffo@gmail.com>
2022-08-09 03:42:20 +0100

    xylophone app completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,20 @@ void playSound(int soundNumber) {
   );
 }
 
+Expanded buildKey({Color? color, int? soundNumber}) {
+  return Expanded(
+    child: Container(
+      color: color,
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber!);
+        },
+        child: Text(''),
+      ),
+    ),
+  );
+}
+
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,83 +37,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: Container(
-                  color: Colors.blue,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(1);
-                    },
-                    child: Text(''),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.green,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(2);
-                    },
-                    child: Text(''),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.yellow,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(3);
-                    },
-                    child: Text(''),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.red,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(4);
-                    },
-                    child: Text(''),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.teal,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(5);
-                    },
-                    child: Text(''),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.orange,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(6);
-                    },
-                    child: Text(''),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.purpleAccent,
-                  child: TextButton(
-                    onPressed: () {
-                      playSound(7);
-                    },
-                    child: Text(''),
-                  ),
-                ),
-              ),
+              buildKey(color: Colors.blue, soundNumber: 1),
+              buildKey(color: Colors.red, soundNumber: 2),
+              buildKey(color: Colors.green, soundNumber: 3),
+              buildKey(color: Colors.yellow, soundNumber: 4),
+              buildKey(color: Colors.orange, soundNumber: 5),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.pink, soundNumber: 7),
             ],
           ),
         ),
