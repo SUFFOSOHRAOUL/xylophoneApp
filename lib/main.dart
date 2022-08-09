055dd1e114d4e91b151b2ddca68bf4af5790faff
unknown <sohraoulsuffo@gmail.com>
2022-08-09 02:58:21 +0100

    using flutter packages to play audio flies

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,102 @@ void main() {
   runApp(XylophoneApp());
 }
 
+void playSound(int soundNumber) {
+  final assetsAudioPlayer = AssetsAudioPlayer();
+
+  assetsAudioPlayer.open(
+    Audio("assets/note$soundNumber.wav"),
+  );
+}
+
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final assetsAudioPlayer = AssetsAudioPlayer();
-
-                assetsAudioPlayer.open(
-                  Audio("assets/note1.wav"),
-                );
-              },
-              child: Text('click me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(1);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(2);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(3);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(4);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.teal,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(5);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(6);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.purpleAccent,
+                  child: TextButton(
+                    onPressed: () {
+                      playSound(7);
+                    },
+                    child: Text(''),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
